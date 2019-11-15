99641aea97d0cf8935cbe05a1ba595e612f7884f
twinandahanur <57204330+twinandahanur@users.noreply.github.com>
2019-11-15 18:28:49 +0700

    Update User.java

--- src/user/User.java
+++ src/user/User.java
@@ -3,4 +3,64 @@ package user;
 import soal.Soal;
 import static soal.Soal.level;
 
-public class User {}
+public class User {
+    
+    
+    // Attribut 
+    private String nama;
+    private int skor;
+    private int lives;
+    private static int level;
+    
+    // Konstruktor
+    public User() {
+        nama = "Kelompok 4";
+        skor = 0;
+        lives = 3;
+        
+    }
+    
+    // Method
+    public void setNama(String inputNama){
+        nama = inputNama;
+    }
+    
+    public String getNama(){
+        return nama;
+    }
+
+    public int getSkor() {
+        return skor;
+    }
+
+    public void setSkor(int skor) {
+        this.skor = skor;
+    }
+
+    public int getLives() {
+        return lives;
+    }
+
+    public void setLives(int lives) {
+        this.lives = lives;
+    }
+    
+        public static int getLevel() {
+        return level;
+    }
+
+    public static void setLevel(int skor) {
+        if (skor <= 100) {
+            level = 1;
+        }
+        
+        else if ((skor >= 101) && (skor <= 200)) {
+            Soal.level = 2;
+        }
+
+        else if ((skor >= 201) && (skor <= 300)) {
+            Soal.level = 3;
+        }
+    }
+    
+}
